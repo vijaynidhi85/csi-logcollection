072e8ca615fd1c04ed699789e58ef6aea5c8d727
Vijay <vijaynidhi.dhayanidh@nutanix.com>
2022-12-29 10:42:21 +1100

    fixed log naming

--- log_collection.sh
+++ log_collection.sh
@@ -13,8 +13,8 @@ kubectl logs -n ntnx-system csi-provisioner-ntnx-plugin-0 -c ntnx-csi-plugin > n
 kubectl logs -n ntnx-system csi-provisioner-ntnx-plugin-0 -c csi-resizer > ntnx-csi-resizer.log
 kubectl logs -n ntnx-system csi-provisioner-ntnx-plugin-0 -c csi-provisioner > csi-provisioner.log
 kubectl logs -n ntnx-system csi-provisioner-ntnx-plugin-0 -c csi-attacher > csi-attacher.log
-kubectl logs -n ntnx-system csi-provisioner-ntnx-plugin-0 -c csi-attacher > csi-snapshotter.log
-kubectl logs -n ntnx-system csi-provisioner-ntnx-plugin-0 -c csi-attacher > csi-external-health-monitor-controller.log
+kubectl logs -n ntnx-system csi-provisioner-ntnx-plugin-0 -c csi-snapshotter > csi-snapshotter.log
+kubectl logs -n ntnx-system csi-provisioner-ntnx-plugin-0 -c csi-external-health-monitor-controller > csi-external-health-monitor-controller.log
 for i in `kubectl get pods -n ntnx-system -l app=csi-node-ntnx-plugin -ojsonpath='{.items[*].metadata.name}'`;do kubectl logs -n ntnx-system $i -c csi-node-ntnx-plugin > csi-node-ntnx-plugin_$i.log;done
 for i in `kubectl get pods -n ntnx-system -l app=csi-node-ntnx-plugin -ojsonpath='{.items[*].metadata.name}'`;do kubectl logs -n ntnx-system $i -c driver-registrar > driver-registrar_$i.log;done
 
